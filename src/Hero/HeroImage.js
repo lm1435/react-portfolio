6c5ea2873e9e5b1beb2eaf70a849883cc60209ad
Luis Meza <luismeza@Luiss-MBP.frontierlocal.net>
2018-07-23 22:24:17 -0400

    updated hero image

--- src/Hero/HeroImage.js
+++ src/Hero/HeroImage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './HeroImage.css';
+import mainImage from '../images/homepage.png'
 
 const HeroImage = () => {
     return(
-        <div className="hero-image">
+        <div className="hero-image" style={{backgroundImage: "url(" + mainImage + ")"}}>
             <div>
                 <p>
                     Luis Meza
