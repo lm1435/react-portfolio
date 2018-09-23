63666dbfe92c5c16625999c447f4d68938f4f58b
Luis Meza <luismeza01@gmail.com>
2018-09-23 15:59:42 -0400

    removing space in the funciton destructuring

--- src/Text/HeaderText.js
+++ src/Text/HeaderText.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './HeaderText.css';
 
-const HeaderText = ({ color='', children } ) => 
+const HeaderText = ({ color='', children }) => 
   <h3 className="header" style={{ 'color': color }}>
     {children}
   </h3>
